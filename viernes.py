b0e9d297627f14d290875241216e0c031c83959c
senatinoprohacker <1475937@senati.pe>
2023-03-03 10:57:10 -0500

    viernes

---
+++ viernes.py
@@ -0,0 +1,11 @@
+# Ingresar la clase math
+import math
+# Ingresar el radio del circulo
+
+print("Ingrese el radio del circulo:")
+r = float(input())
+
+# obtener el vaolr de pi
+
+a = math.pi * (r * r)
+print("El area del circulo con raido ",r," Es: ",round(a, 2))
